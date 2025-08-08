1600120ad55bc667e0ff3a324e0917d9fee48f6a
jerannn24 <junioroppo64@gmail.com>
2025-08-08 10:58:57 +0700

    ADD : global.css

--- app/component/calculator.js
+++ app/component/calculator.js
@@ -25,30 +25,31 @@ const Calculator = () => {
             <h2>CALCULATOR APP</h2>
             <div className='calculator'>
                 <input className="form-control mb-3" type='text' value={result} readOnly></input>
-                <button onClick={() => inputHandle('7')}>7</button>
-                <button onClick={() => inputHandle('8')}>8</button>
-                <button onClick={() => inputHandle('9')}>9</button>
-                <button onClick={() => inputHandle('CE')}>CE</button>
+                <div className='button'>
+                    <button onClick={() => inputHandle('7')}>7</button>
+                    <button onClick={() => inputHandle('8')}>8</button>
+                    <button onClick={() => inputHandle('9')}>9</button>
+                    <button className='operator' onClick={() => inputHandle('CE')}>CE</button>
 
-                <button onClick={() => inputHandle('4')}>4</button>
-                <button onClick={() => inputHandle('5')}>5</button>
-                <button onClick={() => inputHandle('6')}>6</button>
-                <button onClick={() => inputHandle('/')}>/</button>
+                    <button onClick={() => inputHandle('4')}>4</button>
+                    <button onClick={() => inputHandle('5')}>5</button>
+                    <button onClick={() => inputHandle('6')}>6</button>
+                    <button className='operator' onClick={() => inputHandle('/')}>/</button>
 
-                <button onClick={() => inputHandle('1')}>1</button>
-                <button onClick={() => inputHandle('2')}>2</button>
-                <button onClick={() => inputHandle('3')}>3</button>
-                <button onClick={() => inputHandle('*')}>*</button>
+                    <button onClick={() => inputHandle('1')}>1</button>
+                    <button onClick={() => inputHandle('2')}>2</button>
+                    <button onClick={() => inputHandle('3')}>3</button>
+                    <button className='operator' onClick={() => inputHandle('*')}>*</button>
 
-                <button onClick={() => inputHandle('0')}>0</button>
-                <button onClick={() => inputHandle('.')}>.</button>
-                <button onClick={() => inputHandle('00')}>00</button>
-                <button onClick={() => inputHandle('-')}>-</button>
+                    <button onClick={() => inputHandle('0')}>0</button>
+                    <button onClick={() => inputHandle('.')}>.</button>
+                    <button onClick={() => inputHandle('00')}>00</button>
+                    <button className='operator' onClick={() => inputHandle('-')}>-</button>
 
-                <button onClick={() => inputHandle('C')}>C</button>
-                <button onClick={() => inputHandle('=')}>=</button>
-                <button onClick={() => inputHandle('+')}>+</button>
-
+                    <button className='operator wide' id='clear' onClick={() => inputHandle('C')}>C</button>
+                    <button className='operator' onClick={() => inputHandle('=')}>=</button>
+                    <button className='operator' onClick={() => inputHandle('+')}>+</button>
+                </div>
             </div>
         </>
     )
